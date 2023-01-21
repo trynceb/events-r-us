3de5679ae58483caa7bfd03feb896b702e7618e8
Trynce Brandt <tjbrandt2013@gmail.com>
2023-01-21 17:57:53 -0600

    create events model

--- main_app/models.py
+++ main_app/models.py
@@ -1,15 +1,26 @@
 from django.db import models
+from datetime import date
 from django.contrib.auth.models import User
+
+CATEGORIES = (
+    ('CONCERTS', 'Concerts'),
+    ('SPORTS', 'Sports'),
+    ('ARTS_THEATER', 'Arts & Theater')
+)
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-
-
-
-
-
+class Events(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORIES,
+        default=CATEGORIES[0][0]
+    )
+    date = models.DateField(default=date.today)
+    time = models.TimeField()
 
 
 
