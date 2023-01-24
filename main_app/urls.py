0cca0b7946ad012b2983f678a3e7d7715cc7cee7
username <youremail@domain.com>
2023-01-24 13:33:41 -0500

    details fix

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('events/', views.IndexView.as_view(), name='index'),
     path('my-events/', views.MyEventsView.as_view(), name='my_events'),
-    path('events/<int:pk>/', views.EventDetailsView.as_view(), name='details'),
+    path('events/<int:event_id>/', views.EventDetailsView.as_view(), name='details'),
     path('reviews/create/', views.ReviewCreate.as_view(), name='review_create')
 ]
 
